8dc8a2c63bc9b89bd9597a15db7bed73bbe9d58b
nvtrakhmawati <rakhmawati.novita@gmail.com>
2019-06-28 14:12:38 +0700

    add room done

--- client/src/components/cardRoom.vue
+++ client/src/components/cardRoom.vue
@@ -1,18 +1,31 @@
 <template>
-    <div>
-        <b-card bg-variant="dark" text-variant="white" title="Room Name">
-            <b-card-text>
-                Player Who Join the game:
+ <b-container style="box-shadow: 0 0 10px black;background-color: rgba(168, 163, 163, 0.5);">
+      <b-row >
+        <b-col v-for="room in rooms" :key="room.id" cols="4 my-2">
+          <b-card style="background-color: whitesmoke; border: 2px solid grey;" text-variant="black">
+             <b-card-text class="text-center font-weight-bold" style="font-size: 30px;">
+                {{ room.name }}
             </b-card-text>
-            <b-card-text v-for="player in players" :key="player.id">
-                {{}}
-            </b-card-text>
-            <b-button href="#" variant="primary" v-if="start=='true'" @click="start">Start</b-button>
-        </b-card>
-    </div>    
+            <div style="height: 200px;">
+                <b-card-text style="font-size: 25px;">
+                    Players:
+                <b-card-text v-for="(player, index) in room.players" :key="index">
+                    {{player.name}}
+                </b-card-text>
+                </b-card-text>
+            </div>
+            <div class="d-flex justify-content-between"> 
+                <h3> {{ room.players.length }} / {{room.totalPlayers}}</h3>
+                <b-button href="#" variant="primary" @click="join(room.id)" style="width:100px;">JOIN</b-button>
+            </div>
+          </b-card> 
+        </b-col>
+      </b-row>
+    </b-container>
 </template>
 
 <script>
+import db from '@/config/firebase'
 
 export default {
     name:'CardRoom',    
@@ -20,12 +33,33 @@ export default {
         return{
             players: [
                 //ambil dari database
-            ]
+            ],
+            rooms: []
         }
     },
-    methods:{
-        start(){
+    created(){
+        db.collection("rooms")
+          .onSnapshot((querySnapshot) => {
+            let roomList = [];
+            let playerList;
+            querySnapshot.forEach((doc) =>{
+                let obj = {
+                    id: doc.id,
+                    ...doc.data()
+                }
+                
+                roomList.push(obj)
+                // playerList = 
+                
+            });
 
+            this.rooms = roomList
+        });
+
+    },
+    methods:{
+        join(id){
+            this.$router.push(`lobby`, id)
         }
     }
 }
--- client/src/views/Room.vue
+++ client/src/views/Room.vue
@@ -1,52 +1,70 @@
 <template>
-<div>
-    <b-container fluid>
-        <form @submit.prevent="create">
-            <b-row class="my-1">
-                <b-col sm="3">
-                    <label for="roomName" >Room Name :</label>
-                </b-col>
-                <b-col sm="5">
-                    <b-form-input id="`roomName`" type="text" v-model="roomName"></b-form-input>
-                </b-col>
-            </b-row>
-            <b-row class="my-1" >
-                <b-col sm="3">
-                    <label for="roomName" >Amount of Players :</label>
-                </b-col>
-                <b-col sm="5">
-                    <b-form-select
-                        class="mb-2 mr-sm-2 mb-sm-0"
-                        :value="null"
-                        :options="{'2': '2', '3': '3' , '4': '4'}"
-                        id="inline-form-custom-select-pref"
-                    >
-                        <option slot="first" :value="null">Choose...</option>
-                    </b-form-select>
-                </b-col>
-            </b-row>
-            <b-button type="submit" variant="primary" >Create</b-button>
-        </form>
-    </b-container>
-
-    <CardRoom/>
+<div id="room">    
+    <div style="padding-top: 5%;">
+     <b-container style="background-color: #640F21; color: white; padding: 20px;">
+         <form @submit.prevent="create">
+             <b-row class="">
+                 <b-col sm="3">
+                     <label for="roomName" style="font-weight:bold; font-size: 20px;">Room Name :</label>
+                 </b-col>
+                 <b-col sm="5">
+                     <b-form-input id="`roomName`" type="text" v-model="roomName"></b-form-input>
+                 </b-col>
+             </b-row>
+             <b-row class="mt-3" >
+                 <b-col sm="3">
+                     <label for="roomName" style="font-weight:bold; font-size: 20px;">Amount of Players :</label>
+                 </b-col>
+                 <b-col sm="3" class="d-flex justify-content-around">
+                    <b-form-select v-model="totalPlayers" :options="options"></b-form-select>
+                     <b-button type="submit" variant="primary" style="width: 100px;">Create</b-button>
+                 </b-col>
+             </b-row>
+         </form>
+     </b-container>
+   </div>
+     <CardRoom/>
 </div>
 </template>
 
 <script>
 import CardRoom from '@/components/cardRoom.vue'
-// import db from ''
+import db from '@/config/firebase.js'
 
 export default {
     name: 'Room',
     data(){
         return{
-            roomName: ''
+            roomName: '',
+            totalPlayers: null,
+            options: [
+                { value: 2, text: '2' },
+                { value: 3, text: '3' },
+                { value: 4, text: '4' }
+            ]
         }
     },
     methods:{
         create(){
-            this.$router.push('/lobby')
+            let random = Math.floor(Math.random()*3)+ 1 
+            db.collection("rooms").add({
+                name: this.roomName,
+                messages: [],
+                players: [{
+                    name: localStorage.player,
+                    combo: 1,
+                    img: `https://storage.googleapis.com/miniwp_image-storage/dance${random}.gif`
+                }],
+                roomMaster: localStorage.getItem('player'),
+                totalPlayers: this.totalPlayers
+            })
+            .then(function() {
+                console.log('success')
+                this.$router.push('/lobby')
+            })
+            .catch(function(error) {
+                console.log(err)
+            });
         }
     },
     components:{
@@ -56,5 +74,9 @@ export default {
 </script>
 
 <style scoped>
-
+#room {
+   height: 100vh;
+   background: url('https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/yRF5c-O/disco-dance-floor-seamless-vj-loop-motion-background-orange-red-yellow-hot_swj9-3pzl_thumbnail-full01.png');
+   background-size: cover
+ }
 </style>
