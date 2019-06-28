09d8c3003330e7c2678e4de2e0eb02a6282ad3ea
Shandi Yuwono <shandi.yuwono@yahoo.com>
2019-06-28 14:49:25 +0700

    lobby

--- client/src/components/cardRoom.vue
+++ client/src/components/cardRoom.vue
@@ -1,15 +1,23 @@
 <template>
-    <div>
-        <b-card bg-variant="dark" text-variant="white" title="Room Name">
-            <b-card-text>
-                Player Who Join the game:
+    <b-container style="box-shadow: 0 0 10px black;background-color: rgba(168, 163, 163, 0.5);">
+
+      <b-row >
+        <b-col v-for="index in 4" :key="index" cols="4 my-2">
+          <b-card style="background-color: whitesmoke; border: 2px solid grey;" text-variant="black">
+             <b-card-text class="text-center font-weight-bold" style="font-size: 30px;">
+                Room Name
+            </b-card-text>
+            <b-card-text style="height: 200px; font-size: 20px;">
+                Players:
             </b-card-text>
             <b-card-text v-for="player in players" :key="player.id">
                 {{}}
             </b-card-text>
             <b-button href="#" variant="primary" v-if="start=='true'" @click="start">Start</b-button>
-        </b-card>
-    </div>    
+          </b-card> 
+        </b-col>
+      </b-row>
+    </b-container>    
 </template>
 
 <script>
--- client/src/views/Room.vue
+++ client/src/views/Room.vue
@@ -1,35 +1,37 @@
 <template>
-<div>
-    <b-container fluid>
-        <form @submit.prevent="create">
-            <b-row class="my-1">
-                <b-col sm="3">
-                    <label for="roomName" >Room Name :</label>
-                </b-col>
-                <b-col sm="5">
-                    <b-form-input id="`roomName`" type="text" v-model="roomName"></b-form-input>
-                </b-col>
-            </b-row>
-            <b-row class="my-1" >
-                <b-col sm="3">
-                    <label for="roomName" >Amount of Players :</label>
-                </b-col>
-                <b-col sm="5">
-                    <b-form-select
-                        class="mb-2 mr-sm-2 mb-sm-0"
-                        :value="null"
-                        :options="{'2': '2', '3': '3' , '4': '4'}"
-                        id="inline-form-custom-select-pref"
-                    >
-                        <option slot="first" :value="null">Choose...</option>
-                    </b-form-select>
-                </b-col>
-            </b-row>
-            <b-button type="submit" variant="primary" >Create</b-button>
-        </form>
-    </b-container>
-
-    <CardRoom/>
+<div id="room">
+  
+    <div style="padding-top: 5%;">
+      <b-container style="background-color: #640F21; color: white; padding: 20px;">
+          <form @submit.prevent="create">
+              <b-row class="">
+                  <b-col sm="3">
+                      <label for="roomName" style="font-weight:bold; font-size: 20px;">Room Name :</label>
+                  </b-col>
+                  <b-col sm="5">
+                      <b-form-input id="`roomName`" type="text" v-model="roomName"></b-form-input>
+                  </b-col>
+              </b-row>
+              <b-row class="mt-3" >
+                  <b-col sm="3">
+                      <label for="roomName" style="font-weight:bold; font-size: 20px;">Amount of Players :</label>
+                  </b-col>
+                  <b-col sm="3" class="d-flex justify-content-around">
+                      <b-form-select
+                          class="mb-2 mr-sm-2 mb-sm-0"
+                          :value="null"
+                          :options="{'2': '2', '3': '3' , '4': '4'}"
+                          id="inline-form-custom-select-pref"
+                      >
+                          <option slot="first" :value="null">Choose...</option>
+                      </b-form-select>
+                      <b-button type="submit" variant="primary" style="width: 100px;">Create</b-button>
+                  </b-col>
+              </b-row>
+          </form>
+      </b-container>
+    </div>
+      <CardRoom/>
 </div>
 </template>
 
@@ -56,5 +58,9 @@ export default {
 </script>
 
 <style scoped>
-
+  #room {
+    height: 100vh;
+    background: url('https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/yRF5c-O/disco-dance-floor-seamless-vj-loop-motion-background-orange-red-yellow-hot_swj9-3pzl_thumbnail-full01.png');
+    background-size: cover
+  }
 </style>
